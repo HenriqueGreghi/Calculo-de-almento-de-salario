ff6a416d595e037281b6d0d48a8efa10e8079683
Henrique Greghi <henriquevsg@gmail.com>
2022-08-16 16:41:02 -0300

    Calculando aumento

---
+++ AumentoDeSalario/CalculoDeAumento.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AumentoDeSalario
+{
+    class CalculoDeAumento
+    {
+        public double Salario;
+        public double Total;
+        public double ValorAumento;
+
+        public void Calculo()
+        {
+            if (Salario <= 400.00)
+            {
+                Total = Salario + Salario * 0.15;
+                ValorAumento = Total - Salario;
+                Console.WriteLine("Novo salario: " + Total.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + ValorAumento.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual : 15%");
+
+            }
+            else if (Salario <= 800.00)
+            {
+                Total = Salario + Salario * 0.12;
+                ValorAumento = Total - Salario;
+                Console.WriteLine("Novo salario: " + Total.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + ValorAumento.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual : 12%");
+
+            }
+            else if (Salario <= 1200.00)
+            {
+                Total = Salario + Salario * 0.10;
+                ValorAumento = Total - Salario;
+                Console.WriteLine("Novo salario: " + Total.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + ValorAumento.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual : 10%");
+
+            }
+            else if (Salario <= 2000.00)
+            {
+                Total = Salario + Salario * 0.07;
+                ValorAumento = Total - Salario;
+                Console.WriteLine("Novo salario: " + Total.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + ValorAumento.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual : 7%");
+
+            }
+            else if(Salario >= 2000.01)
+            {
+                Total = Salario + Salario * 0.04;
+                ValorAumento = Total - Salario;
+                Console.WriteLine("Novo salario: " + Total.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Reajuste ganho: " + ValorAumento.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Em percentual : 4%");
+            }
+            else { }
+        }
+    }
+
+}
---
+++ AumentoDeSalario/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace AumentoDeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculoDeAumento valor = new CalculoDeAumento();
+
+            Console.WriteLine("Entre com o seu salario: ");
+            valor.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            valor.Calculo();
+        }
+    }
+}
